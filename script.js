6f44bf686ae9c0d9593d54c6ba0aa79b9a6f42c9
Wanilinka <mangausov@gmail.com>
2024-06-29 21:10:31 +0300

    First code

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+// Додаємо анімацію для кнопки пошуку
+document.addEventListener('DOMContentLoaded', function() {
+    var searchInput = document.getElementById('searchInput');
+
+    searchInput.addEventListener('focus', function() {
+        searchInput.style.transition = 'all 0.5s ease';
+        searchInput.style.width = '300px'; // Збільшуємо ширину при фокусі
+    });
+
+    searchInput.addEventListener('blur', function() {
+        searchInput.style.transition = 'all 0.5s ease';
+        searchInput.style.width = '200px'; // Повертаємося до початкової ширини при втраті фокусу
+    });
+});
