f35873fad69723cb5d7ce34a3bcc53c52c10e4be
Wanilinka <mangausov@gmail.com>
2024-07-01 18:17:49 +0300

    Site V0.1
    @Wanilinka

--- script.js
+++ script.js
@@ -2,13 +2,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     var searchInput = document.getElementById('searchInput');
 
-    searchInput.addEventListener('focus', function() {
-        searchInput.style.transition = 'all 0.5s ease';
-        searchInput.style.width = '300px'; // Збільшуємо ширину при фокусі
-    });
+    if (searchInput) {
+        searchInput.addEventListener('focus', function() {
+            searchInput.style.transition = 'all 0.5s ease';
+            searchInput.style.width = '300px'; // Збільшуємо ширину при фокусі
+        });
 
-    searchInput.addEventListener('blur', function() {
-        searchInput.style.transition = 'all 0.5s ease';
-        searchInput.style.width = '200px'; // Повертаємося до початкової ширини при втраті фокусу
-    });
+        searchInput.addEventListener('blur', function() {
+            searchInput.style.transition = 'all 0.5s ease';
+            searchInput.style.width = '200px'; // Повертаємося до початкової ширини при втраті фокусу
+        });
+    }
+
+    var loginButton = document.getElementById('loginButton');
+
+    if (loginButton) {
+        loginButton.addEventListener('click', function() {
+            var email = document.getElementById('emailInput').value;
+            var password = document.getElementById('passwordInput').value;
+
+            // Перевірка, чи введені значення (тут можна додати більше умов для перевірки)
+            if (email && password) {
+                // Перенаправлення на сторінку панелі управління
+                window.location.href = 'dashboard.html';
+            } else {
+                alert('Please enter both email and password');
+            }
+        });
+    }
 });
